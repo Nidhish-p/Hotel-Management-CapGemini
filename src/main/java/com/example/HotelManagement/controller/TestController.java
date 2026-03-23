993d905f3b2cb665105fa576fdeb43778440f734
Prajyot Gorlewar <prajyotgorlewar@gmail.com>
2026-03-23 16:51:18 +0530

    adde controller

---
+++ src/main/java/com/example/HotelManagement/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.example.HotelManagement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/test")
+@RestController
+public class TestController {
+
+    @GetMapping("/prajyot")
+    public String getPrajyot() {
+        return "Prajyot";
+    }
+
+}
