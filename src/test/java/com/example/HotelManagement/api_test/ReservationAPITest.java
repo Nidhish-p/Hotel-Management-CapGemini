cb4dc088455ced96ddd4835a333be4b211e6b84b
tanwee14 <8409tan@gmail.com>
2026-03-25 20:41:08 +0530

    added tests

--- src/test/java/com/example/HotelManagement/api_test/ReservationAPITest.java
+++ src/test/java/com/example/HotelManagement/api_test/ReservationAPITest.java
@@ -1,8 +1,5 @@
-package com.example.HotelManagement;
+package com.example.HotelManagement.api_test;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
@@ -12,28 +9,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.example.HotelManagement.entity.Reservation;
-import com.example.HotelManagement.repository.ReservationRepository;
-
-
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-class ReservationRepositoryTest {
+class ReservationAPITest {
 
     @Autowired
-    private ReservationRepository reservationRepository;
-
-    @Autowired
-    MockMvc mockMvc;
-
-    @Test
-    // @DisplayName("Get All Reservations Test")
-    void getAllReservationsTestRepo() {
-        List<Reservation> reservations = reservationRepository.findAll();
-        assertThat(reservations).isNotNull();
-        assertThat(reservations).isNotEmpty(); // since data already exists
-    }
+    private MockMvc mockMvc;
 
     @Test
     void getAllReservationsTestApi() throws Exception {
