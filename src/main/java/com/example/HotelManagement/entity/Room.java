d34b313a3379389c091d0f0b1312eace3edd7841
Night7407 <164714112+Night7407@users.noreply.github.com>
2026-03-27 14:27:56 +0530

    fixed

--- src/main/java/com/example/HotelManagement/entity/Room.java
+++ src/main/java/com/example/HotelManagement/entity/Room.java
@@ -2,9 +2,7 @@ package com.example.HotelManagement.entity;
 
 import java.util.List;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
