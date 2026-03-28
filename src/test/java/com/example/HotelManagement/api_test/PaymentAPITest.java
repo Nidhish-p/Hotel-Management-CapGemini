96939f8fa1c2420d9a26e98c02db6b36c3307d83
harshal borkar <harshal.deepak.borkar@gmail.com>
2026-03-28 15:50:29 +0530

    added test cases in PaymentAPITest

--- src/test/java/com/example/HotelManagement/api_test/PaymentAPITest.java
+++ src/test/java/com/example/HotelManagement/api_test/PaymentAPITest.java
@@ -2,6 +2,8 @@ package com.example.HotelManagement.api_test;
 
 import com.example.HotelManagement.entity.Payment;
 import com.example.HotelManagement.entity.Reservation;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.http.MediaType;
 import org.junit.jupiter.api.Test;
@@ -9,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
 
 import java.sql.Date;
 import java.time.LocalDate;
@@ -20,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 public class PaymentAPITest {
 
     @Autowired
@@ -42,50 +47,19 @@ public class PaymentAPITest {
     }
 
     @Test
-    public void testGetPaymentById() throws Exception{
-        String json = """
-                {
-                    "amount":12389,
-                    "payment_date":"2026-03-23",
-                    "payment_status":"Pending"
-                }
-                """;
-
-        String location = mockMvc.perform(post("/payments")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(json))
-                        .andExpect(status().isCreated())
-                        .andExpect(header().exists("Location"))
-                        .andReturn()
-                        .getResponse()
-                        .getHeader("Location");
-
-        mockMvc.perform(get(location))
-                .andExpect(status().isOk());
-    }
-
-    @Test
     public void testCreatePayment() throws Exception{
         String json = """
                 {
-                    "amount":12389,
-                    "payment_date":"2026-03-23",
-                    "payment_status":"Pending"
+                    "amount": 2377.00,
+                    "payment_date": "2024-01-15",
+                    "payment_status": "CD",
+                    "reservation":"http://localhost:8081/reservations/1"
                 }
                 """;
-
         mockMvc.perform(post("/payments")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(json))
-                        .andExpect(status().isCreated())
-                        .andExpect(header().exists("Location"));
-
-    }
-
-
-    @Test
-    public void testCreatePaymentWithNegativeAmount() throws Exception{
-
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isCreated());
     }
 
     @Test
@@ -95,22 +69,27 @@ public class PaymentAPITest {
     }
 
     @Test
-    public void testUpdatePayment() throws Exception {
-
-        String json = """
-                {
-                   "amount": 3000,
-                   "payment_date": "2026-03-23",
-                   "payment_status": "Paid",
-                   "reservation_id": 1
-                 }
-                """;
-
-        mockMvc.perform(put("/payments/1")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(json))
+    public void testGetPaymentById() throws Exception{
+        mockMvc.perform(get("/payments/1")
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
+
+//    @Test
+//    public void testUpdatePaymentById() throws Exception{
+//        String json = """
+//                {
+//                   "amount": 3000,
+//                   "payment_date": "2026-03-23",
+//                   "payment_status": "Paid",
+//                   "reservation": "http://localhost:8081/reservations/1"
+//                 }
+//                """;
+//        mockMvc.perform(put("/payments/1")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(json))
+//                .andExpect(status().isOk());
+//    }
 
     @Test
     public void testDeletePayment() throws Exception {
@@ -125,14 +104,32 @@ public class PaymentAPITest {
         String location = mockMvc.perform(post("/payments")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(json))
-                        .andExpect(status().isCreated())
-                        .andExpect(header().exists("Location"))
-                        .andReturn()
-                        .getResponse()
-                        .getHeader("Location");
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"))
+                .andReturn()
+                .getResponse()
+                .getHeader("Location");
 
         mockMvc.perform(delete(location))
                 .andExpect(status().isNoContent());
     }
+
+    @Test
+    void testCreatePayment_InvalidData() throws Exception {
+        String json = """
+                {
+                    "amount": -500.00,
+                    "payment_date": "2026-03-28",
+                    "payment_status": "FAILED",
+                    "reservation": "/reservations/%d"
+                }
+                """.formatted(testReservation.getReservation_id());
+
+        mockMvc.perform(post("/payments")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isBadRequest());
+    }
+
 
 }
