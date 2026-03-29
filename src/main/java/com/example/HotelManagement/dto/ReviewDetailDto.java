db4f6d5821fef75cd706a920c41325445fed9ef2
Zaid Syed <zaidjaved04@gmail.com>
2026-03-29 13:02:31 +0530

    Review Final page with some test cases

--- src/main/java/com/example/HotelManagement/dto/ReviewDetailDto.java
+++ src/main/java/com/example/HotelManagement/dto/ReviewDetailDto.java
@@ -1,35 +1,27 @@
 package com.example.HotelManagement.dto;
 
 import com.example.HotelManagement.entity.Review;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.rest.core.config.Projection;
 
 import java.time.LocalDate;
-@Projection(name = "reviewDetails", types = Review.class)
-public interface ReviewDetailDto {
-    // Reservation fields
-    /*
-    String getReservation_Guest_name();
-    LocalDate getReservation_Check_in_date();
-    LocalDate getReservation_Check_out_date();
-    Integer getReservation_Reservation_id();
+@Data
+@Getter
+@Setter
+public class ReviewDetailDto {
+    private Integer reviewId;
+    private String comment;
+    private int rating;
+    private LocalDate reviewDate;
 
-    // Room fields
-    Integer getReservation_Room_RoomNumber();
+    private String guestName;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
 
-    // ✅ RoomType name
-    String getReservation_Room_RoomType_Type_name();
+    private Integer roomNumber;
+    private String roomType;
 
-     */
-    // 🔹 Reservation fields
-    String getReservation_GuestName();
-    LocalDate getReservation_CheckInDate();
-    LocalDate getReservation_CheckOutDate();
-    Integer getReservation_Reservation_id();
-
-    // 🔹 Room fields
-    Integer getReservation_Room_RoomNumber();
-
-    // 🔹 RoomType field
-    String getReservation_Room_RoomType_TypeName();
 }
 
