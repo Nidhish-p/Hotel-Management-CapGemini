a108ffc93253ebf654c00ae0783ecff25e45ffc5
nidhish <nidhish17parke@gmail.com>
2026-03-31 11:03:36 +0530

    Complete api tests with exception handlers

--- src/main/java/com/example/HotelManagement/repository/RoomRepository.java
+++ src/main/java/com/example/HotelManagement/repository/RoomRepository.java
@@ -2,6 +2,8 @@ package com.example.HotelManagement.repository;
 
 import com.example.HotelManagement.dto.RoomSummary;
 import com.example.HotelManagement.entity.Room;
+import com.example.HotelManagement.entity.RoomType;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -11,4 +13,5 @@ import java.util.List;
 
 @RepositoryRestResource(excerptProjection = RoomSummary.class)
 public interface RoomRepository extends JpaRepository<Room, Integer> {
+    boolean existsByRoomType(RoomType roomType);
 }
